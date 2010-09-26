0/snake

commit f236e3226506879d3399fca727225490a70e866c

Corrected reporting of game duration

snake.c
@@ -68,7 +68,7 @@ typedef struct {
 } block_t;
 
 
-time_t time_start;
+time_t time_start, time_stop;
 unsigned int frame_wait;
 
 direction_t dir;
@@ -493,6 +493,7 @@ int main (int argc, char **argv) {
 		usleep (frame_wait * (NORTH == dir || SOUTH == dir ? V_COEF : H_COEF));
 	}
 
+	time_stop = time (NULL);
 	mvaddstr (HEAD.y, HEAD.x, "DEAD!");
 
 	nodelay (stdscr, FALSE);
@@ -505,7 +506,7 @@ int main (int argc, char **argv) {
 
 
 static void finish (int sig) {
-	double time_total = difftime(time (NULL), time_start);
+	double time_total = difftime(time_stop, time_start);
 
 	free (snake);
 	endwin ();

commit 1e3e13d8eaa7119f246e881fca93acc1696f9dba

Fixed bug introduced in f236e32
Total time was reported incorrectly when quitting using KEY_QUIT since time_stop was unset.

snake.c
@@ -85,6 +85,8 @@ int use_color = 0;
 
 unsigned int frame_min;
 
+int proper_exit = 0;
+
 
 static void finish (int sig);
 
@@ -494,6 +496,7 @@ int main (int argc, char **argv) {
 	}
 
 	time_stop = time (NULL);
+	proper_exit = 1;
 	mvaddstr (HEAD.y, HEAD.x, "DEAD!");
 
 	nodelay (stdscr, FALSE);
@@ -506,7 +509,12 @@ int main (int argc, char **argv) {
 
 
 static void finish (int sig) {
-	double time_total = difftime(time_stop, time_start);
+	double time_total;
+
+	if (!proper_exit)
+		time_stop = time (NULL);
+
+	time_total = difftime(time_stop, time_start);
 
 	free (snake);
 	endwin ();