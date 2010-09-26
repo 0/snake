3a68edb1210c01eebf1d48e4a6ff8970b3dde2a4
user <user>
2010-09-26 13:15:52 -0400

    Reformatted everything for sanity and consistency

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-CFLAGS=-lcurses
+CFLAGS = -lcurses
 
-snake : snake.c
+snake: snake.c
--- snake.c
+++ snake.c
@@ -7,36 +7,34 @@
 #include <time.h>
 #include <unistd.h>
 
+#define CH_HEAD   '@'
+#define CH_BODY   '#'
+#define CH_FOOD   '*'
+#define CH_PORTAL '^'
+#define CH_VOID   ' '
 
-#define LEAD	'@'
-#define BLOCK	'#'
-#define FOOD	'*'
-#define PORTAL	'^'
-#define VOID	' '
+#define HEAD snake[0]
+#define TAIL snake[len]
 
-#define HEAD	snake [0]
-#define TAIL	snake [len]
+#define KEY_UP_ALT    'w'
+#define KEY_RIGHT_ALT 'd'
+#define KEY_DOWN_ALT  's'
+#define KEY_LEFT_ALT  'a'
+#define KEY_QUIT      'q'
 
-#define KEY_UP_ALT		'w'
-#define KEY_RIGHT_ALT	'd'
-#define KEY_DOWN_ALT	's'
-#define KEY_LEFT_ALT	'a'
-#define KEY_QUIT		'q'
+#define START_LEN  0
+#define GROWTH_MIN 10
+#define GROWTH_MAX 100
+#define MAX_LEN    10000
 
-#define START_LEN	0
-#define GROWTH_MIN	10
-#define GROWTH_MAX	100
-#define MAX_LEN		10000
-
-#define FPS_MIN		1
-#define FPS_MAX		1000
+#define FPS_MIN       1
+#define FPS_MAX       1000
 #define FPS_MAX_CHARS 4
-#define FPS_INIT	80
-#define FRAME_DIFF	5
+#define FPS_INIT      80
+#define FRAME_DIFF    5
 
-#define H_COEF		2
-#define V_COEF		3
-
+#define H_COEF 2
+#define V_COEF 3
 
 typedef enum {
 	DEAD,
@@ -61,12 +59,10 @@ typedef enum {
 	DEATH_QUIT
 } cod_t;
 
-
 typedef struct {
 	int x;
 	int y;
 } block_t;
-
 
 time_t time_start, time_stop;
 unsigned int frame_wait;
@@ -87,71 +83,118 @@ unsigned int frame_min;
 
 int proper_exit = 0;
 
+unsigned int delay_to_fps(unsigned int delay) {
+	if (delay > 0)
+		return 2 * 1000000 / (delay * (H_COEF + V_COEF));
+	else
+		return -1;
+}
 
-static void finish (int sig);
+unsigned int fps_to_delay(unsigned int fps) {
+	if (fps > 0)
+		return 2 * 1000000 / (fps * (H_COEF + V_COEF));
+	else
+		return -1;
+}
 
+const char *stringCOD(cod_t cause) {
+	switch (cause) {
+		case DEATH_SELF:
+			return "hit yourself";
+		case DEATH_REVERSE:
+			return "reversed into yourself";
+		case DEATH_PORTAL:
+			return "crashed into a portal";
+		case DEATH_QUIT:
+			return "quit";
+		default:
+			return "died mysteriously";
+	}
+}
 
-void do_color (item_color_t c, int on) {
+static void finish(int sig) {
+	double time_total;
+
+	if (!proper_exit)
+		time_stop = time(NULL);
+
+	time_total = difftime(time_stop, time_start);
+
+	free(snake);
+	endwin();
+
+	if (time_total <= 0 || LINES <= 0 || COLS <= 0) {
+		printf("You didn't even play!\n");
+	} else {
+		const char *cause = stringCOD(cause_of_death);
+		printf("%s with %d segments in %.0f seconds on %d lines and %d columns at %d frames per second\n",
+			cause, len, time_total, LINES, COLS, delay_to_fps(frame_wait));
+	}
+
+	exit(sig ? 1 : 0);
+}
+
+void do_color(item_color_t c, int on) {
 	if (use_color) {
 		if (on) {
-			attron (COLOR_PAIR (c));
+			attron(COLOR_PAIR(c));
 		} else {
-			attroff (COLOR_PAIR (c));
+			attroff(COLOR_PAIR(c));
 		}
 	}
 }
 
-int collideWithSnake (int x, int y) {
+int collideWithSnake(int x, int y) {
 	unsigned int i;
 	for (i = 1; i <= len; ++i) {
-		if (snake [i].x == x && snake [i].y == y)
+		if (snake[i].x == x && snake[i].y == y)
 			return 1;
 	}
 	return 0;
 }
 
-void placeFood () {
+void placeFood() {
 	int done = 0;
 	while (!done) {
-		food.x = rand () % COLS;
-		food.y = rand () % LINES;
-		if (0 == collideWithSnake (food.x, food.y) && food.x != portal.x && portal.y != food.y)
+		food.x = rand() % COLS;
+		food.y = rand() % LINES;
+		if (0 == collideWithSnake(food.x, food.y) && food.x != portal.x && portal.y != food.y)
 			done = 1;
 	}
-	do_color (COLOR_FOOD, 1);
-	mvaddch (food.y, food.x, FOOD);
-	do_color (COLOR_FOOD, 0);
+	do_color(COLOR_FOOD, 1);
+	mvaddch(food.y, food.x, CH_FOOD);
+	do_color(COLOR_FOOD, 0);
 
 	done = 0;
 	while (!done) {
-		portal.x = rand () % COLS;
-		portal.y = rand () % LINES;
-		if (0 == collideWithSnake (portal.x, portal.y) && food.x != portal.x && food.y != portal.y)
+		portal.x = rand() % COLS;
+		portal.y = rand() % LINES;
+		if (0 == collideWithSnake(portal.x, portal.y) && food.x != portal.x && food.y != portal.y)
 			done = 1;
 	}
-	do_color (COLOR_PORTAL, 1);
-	mvaddch (portal.y, portal.x, PORTAL);
-	do_color (COLOR_PORTAL, 0);
+	do_color(COLOR_PORTAL, 1);
+	mvaddch(portal.y, portal.x, CH_PORTAL);
+	do_color(COLOR_PORTAL, 0);
 }
 
-block_t* fetchSnake () {
-	block_t *tmp = malloc ((1 + MAX_LEN) * sizeof (block_t));
-	if (tmp == NULL) {
-		fprintf (stderr, "Can't malloc for the snake!\n");
-		finish (0);
+block_t *fetchSnake() {
+	block_t *tmp = malloc((1 + MAX_LEN) * sizeof(block_t));
+	if (NULL == tmp) {
+		fprintf(stderr, "Can't malloc for the snake!\n");
+		finish(0);
 	}
 	return tmp;
 }
 
-unsigned int extendSnake (unsigned int length) {
+unsigned int extendSnake(unsigned int length) {
 	unsigned int max_increase = MAX_LEN - len;
 	unsigned int increase = length < max_increase ? length : max_increase;
 
 	unsigned int i;
 
 	for (i = 1; i <= increase; ++i) {
-		snake [len + i].x = TAIL.x;
-		snake [len + i].y = TAIL.y;
+		snake[len + i].x = TAIL.x;
+		snake[len + i].y = TAIL.y;
 	}
 
 	len += increase;
@@ -159,15 +202,16 @@ unsigned int extendSnake (unsigned int length) {
 	return length - increase;
 }
 
-int moveSnake () {
+int moveSnake() {
 	int i;
 
-	mvaddch (TAIL.y, TAIL.x, VOID);
+	mvaddch(TAIL.y, TAIL.x, CH_VOID);
 
 	for (i = len; i > 0; --i) {
-		snake [i].x = snake [i - 1].x;
-		snake [i].y = snake [i - 1].y;
-	}	
+		snake[i].x = snake[i - 1].x;
+		snake[i].y = snake[i - 1].y;
+	}
+
 	switch (dir) {
 		case NORTH:
 			HEAD.y--;
@@ -192,45 +236,46 @@ int moveSnake () {
 		default:
 			break;
 	}
-	if (1 == collideWithSnake (HEAD.x, HEAD.y))
+
+	if (1 == collideWithSnake(HEAD.x, HEAD.y))
 		return 1;
 	if (HEAD.x == portal.x && HEAD.y == portal.y)
 		return 2;
 	if (HEAD.x == food.x && HEAD.y == food.y) {
-		extendSnake (rand () % (GROWTH_MAX - GROWTH_MIN) + GROWTH_MIN);
+		extendSnake(rand() % (GROWTH_MAX - GROWTH_MIN) + GROWTH_MIN);
 		HEAD.x = portal.x;
 		HEAD.y = portal.y;
-		mvaddch (food.y, food.x, VOID);
-		placeFood ();
+		mvaddch(food.y, food.x, CH_VOID);
+		placeFood();
 	}
 
-	do_color (COLOR_LEAD, 1);
-	mvaddch (HEAD.y, HEAD.x, LEAD);
-	do_color (COLOR_LEAD, 0);
+	do_color(COLOR_LEAD, 1);
+	mvaddch(HEAD.y, HEAD.x, CH_HEAD);
+	do_color(COLOR_LEAD, 0);
 
 	if (len > 0) {
-		do_color (COLOR_BLOCK, 1);
-		mvaddch (snake [1].y, snake [1].x, BLOCK);
-		do_color (COLOR_BLOCK, 0);
+		do_color(COLOR_BLOCK, 1);
+		mvaddch(snake[1].y, snake[1].x, CH_BODY);
+		do_color(COLOR_BLOCK, 0);
 	}
 
 	return 0;
 }
 
-block_t* reverseSnake () {
+block_t *reverseSnake() {
 	unsigned int i;
-	block_t *tmp = fetchSnake ();
+	block_t *tmp = fetchSnake();
 	for (i = 0; i <= len; ++i) {
-		tmp [i].x = snake [len - i].x;
-		tmp [i].y = snake [len - i].y;
+		tmp[i].x = snake[len - i].x;
+		tmp[i].y = snake[len - i].y;
 	}
-	free (snake);
+	free(snake);
 
 	rev = 1;
 	return tmp;
 }
 
-void speedUp () {
+void speedUp() {
 	if (frame_wait >= FRAME_DIFF) {
 		frame_wait -= FRAME_DIFF;
 		if (frame_wait < frame_min)
@@ -238,37 +283,8 @@ void speedUp () {
 	}
 }
 
-unsigned int delay_to_fps (unsigned int delay) {
-	if (delay > 0)
-		return 2 * 1000000 / (delay * (H_COEF + V_COEF));
-	else
-		return -1;
-}
-
-unsigned int fps_to_delay (unsigned int fps) {
-	if (fps > 0)
-		return 2 * 1000000 / (fps * (H_COEF + V_COEF));
-	else
-		return -1;
-}
-
-const char* stringCOD (cod_t cause) {
-	switch (cause) {
-		case DEATH_SELF:
-			return "hit yourself";
-		case DEATH_REVERSE:
-			return "reversed into yourself";
-		case DEATH_PORTAL:
-			return "crashed into a portal";
-		case DEATH_QUIT:
-			return "quit";
-		default:
-			return "died mysteriously";
-	}
-}
-
-const char* generate_header () {
-	switch (rand () % 10) {
+const char *generate_header() {
+	switch (rand() % 10) {
 		case 0:
 			return "                 _        \n"
 				" ___ _ __   __ _| | _____ \n"
@@ -288,13 +304,16 @@ const char* generate_header () {
 	}
 }
 
-void show_usage (char *cmd) {
-	printf (
+void show_usage(char *cmd) {
+	printf(
 "%s\n"
 "\n"
 "USAGE: %s [options]\n"
 "\n"
-"OPTIONS:\n"
+"OPTIONS:\n",
+		generate_header(), cmd);
+
+	printf(
 "  Display\n"
 "    --bright\n"
 "        Brighter object coloring. Default off.\n"
@@ -303,24 +322,30 @@ void show_usage (char *cmd) {
 "    --instructions\n"
 "        Show instructions. Default off.\n"
 "    --fps-display\n"
-"        Show current framerate. Default off.\n"
+"        Show current framerate. Default off.\n");
+
+	printf(
 "  Speed\n"
 "    --fps-init <num>\n"
 "        Initial framerate (integer). Default: %d\n"
 "    --fps-max <num>\n"
-"        Maximum framerate (integer). Default: %d\n"
+"        Maximum framerate (integer). Default: %d\n",
+		FPS_INIT, FPS_MAX);
+
+	printf(
 "  Miscellaneous\n"
 "    --help\n"
 "        Print this and exit.\n"
 "    --no-<foo>\n"
-"        Disable boolean option <foo>.\n"
+"        Disable boolean option <foo>.\n");
+
+	printf(
 "\n"
-"Framerates must be between %d and %d, inclusive.\n"
-		, generate_header (), cmd, FPS_INIT, FPS_MAX, FPS_MIN, FPS_MAX);
+"Framerates must be between %d and %d, inclusive.\n",
+		FPS_MIN, FPS_MAX);
 }
 
-
-int main (int argc, char **argv) {
+int main(int argc, char **argv) {
 	static int color_flag = 1;
 	static int bright_flag = 0;
 	static int instructions_flag = 0;
@@ -329,7 +354,7 @@ int main (int argc, char **argv) {
 	int fps_init = FPS_INIT;
 	int fps_max = FPS_MAX;
 
-	struct option longopts [] = {
+	struct option longopts[] = {
 		{"bright", no_argument, &bright_flag, 1},
 		{"no-bright", no_argument, &bright_flag, 0},
 		{"color", no_argument, &color_flag, 1},
@@ -349,27 +374,27 @@ int main (int argc, char **argv) {
 
 	char fps_display_buf[FPS_MAX_CHARS + 1];
 
-	srand (time (NULL));
+	srand(time(NULL));
 
-	do {
+	while (c != -1) {
 		char *p;
-		c = getopt_long (argc, argv, "", longopts, NULL);
+		c = getopt_long(argc, argv, "", longopts, NULL);
 
 		switch (c) {
 			case 'h':
-				show_usage (argv[0]);
+				show_usage(argv[0]);
 				exit(0);
 			case 'i':
 				fps_init = strtol(optarg, &p, 10);
 				if (errno || *p || fps_init < FPS_MIN || fps_init > FPS_MAX) {
-					fprintf (stderr, "Invalid value for fps-init: %s\n", optarg);
+					fprintf(stderr, "Invalid value for fps-init: %s\n", optarg);
 					opterr_flag = 1;
 				}
 				break;
 			case 'm':
 				fps_max = strtol(optarg, &p, 10);
 				if (errno || *p || fps_max < FPS_MIN || fps_max > FPS_MAX) {
-					fprintf (stderr, "Invalid value for fps-max: %s\n", optarg);
+					fprintf(stderr, "Invalid value for fps-max: %s\n", optarg);
 					opterr_flag = 1;
 				}
 				break;
@@ -377,27 +402,27 @@ int main (int argc, char **argv) {
 				opterr_flag = 1;
 				break;
 		}
-	} while (c != -1);
+	}
 
 	if (fps_init > fps_max) {
-		fprintf (stderr, "Initial frame rate cannot be higher than the maximum.\n");
+		fprintf(stderr, "Initial frame rate cannot be higher than the maximum.\n");
 		opterr_flag = 1;
 	}
 
 	if (opterr_flag) {
-		fprintf (stderr, "Use --help for usage information.\n");
-		exit (1);
+		fprintf(stderr, "Use --help for usage information.\n");
+		exit(1);
 	}
 
-	signal (SIGINT, finish);
+	signal(SIGINT, finish);
 
-	initscr ();
-	keypad (stdscr, TRUE);
-	nodelay (stdscr, TRUE);
-	curs_set (0);
-	noecho ();
+	initscr();
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, TRUE);
+	curs_set(0);
+	noecho();
 
-	if (color_flag && has_colors ()) {
+	if (color_flag && has_colors()) {
 		use_color = 1;
 		start_color();
 		init_pair(COLOR_LEAD, COLOR_BLUE, bright_flag ? COLOR_BLUE : COLOR_BLACK);
@@ -407,72 +432,73 @@ int main (int argc, char **argv) {
 	}
 
 	if (instructions_flag) {
-		mvaddstr (0, 0, "move: arrows or wasd");
-		mvaddstr (1, 0, "stop: q or die");
-		mvaddstr (2, 0, "quit: any key");
+		mvaddstr(0, 0, "move: arrows or wasd");
+		mvaddstr(1, 0, "stop: q or die");
+		mvaddstr(2, 0, "quit: any key");
 	}
 
-	attron (A_BOLD);
+	attron(A_BOLD);
 
 	len = 0;
-	frame_wait = fps_to_delay (fps_init);
-	frame_min = fps_to_delay (fps_max);
+	frame_wait = fps_to_delay(fps_init);
+	frame_min = fps_to_delay(fps_max);
 
-	snake = fetchSnake ();
+	snake = fetchSnake();
 
 	HEAD.x = COLS / 2;
 	HEAD.y = LINES / 2;
 
 #if START_LEN > 0
-	extendSnake (START_LEN);
+	extendSnake(START_LEN);
 #endif
 
-	placeFood ();
+	placeFood();
 
-	dir = NORTH + rand () % 4;
+	dir = NORTH + rand() % 4;
 
-	time_start = time (NULL);
+	time_start = time(NULL);
 
 	while (dir != DEAD) {
 		rev = 0;
 
-		switch (getch ()) {
+		switch (getch()) {
 			case KEY_UP:
 			case KEY_UP_ALT:
-				if (dir == SOUTH)
-					snake = reverseSnake ();
+				if (SOUTH == dir)
+					snake = reverseSnake();
 				dir = NORTH;
-				speedUp ();
+				speedUp();
 				break;
 			case KEY_RIGHT:
 			case KEY_RIGHT_ALT:
-				if (dir == WEST)
-					snake = reverseSnake ();
+				if (WEST == dir)
+					snake = reverseSnake();
 				dir = EAST;
-				speedUp ();
+				speedUp();
 				break;
 			case KEY_DOWN:
 			case KEY_DOWN_ALT:
-				if (dir == NORTH)
-					snake = reverseSnake ();
+				if (NORTH == dir)
+					snake = reverseSnake();
 				dir = SOUTH;
-				speedUp ();
+				speedUp();
 				break;
 			case KEY_LEFT:
 			case KEY_LEFT_ALT:
-				if (dir == EAST)
-					snake = reverseSnake ();
+				if (EAST == dir)
+					snake = reverseSnake();
 				dir = WEST;
-				speedUp ();
+				speedUp();
 				break;
 			case KEY_QUIT:
 				cause_of_death = DEATH_QUIT;
-				finish (0);
+				finish(0);
 				break;
 			default:
 				break;
 		}
-		switch (moveSnake ()) {
+
+		switch (moveSnake()) {
 			case 1:
 				if (rev)
 					cause_of_death = DEATH_REVERSE;
@@ -490,42 +516,19 @@ int main (int argc, char **argv) {
 
 		if (fps_display_flag) {
 			sprintf(fps_display_buf, "%d", delay_to_fps(frame_wait));
-			mvaddstr (LINES - 1, 0, fps_display_buf);
+			mvaddstr(LINES - 1, 0, fps_display_buf);
 		}
-		usleep (frame_wait * (NORTH == dir || SOUTH == dir ? V_COEF : H_COEF));
+		usleep(frame_wait * (NORTH == dir || SOUTH == dir ? V_COEF : H_COEF));
 	}
 
-	time_stop = time (NULL);
+	time_stop = time(NULL);
 	proper_exit = 1;
-	mvaddstr (HEAD.y, HEAD.x, "DEAD!");
+	mvaddstr(HEAD.y, HEAD.x, "DEAD!");
 
-	nodelay (stdscr, FALSE);
-	getch ();
+	nodelay(stdscr, FALSE);
+	getch();
 
-	finish (0);
+	finish(0);
 
 	return 0;
-}
-
-
-static void finish (int sig) {
-	double time_total;
-
-	if (!proper_exit)
-		time_stop = time (NULL);
-
-	time_total = difftime(time_stop, time_start);
-
-	free (snake);
-	endwin ();
-
-	if (time_total <= 0 || LINES <= 0 || COLS <= 0) {
-		printf ("You didn't even play!\n");
-	} else {
-		const char* cause = stringCOD (cause_of_death);
-		printf ("%s with %d segments in %.0f seconds on %d lines and %d columns at %d frames per second\n",
-			cause, len, time_total, LINES, COLS, delay_to_fps (frame_wait));
-	}
-
-	exit (sig ? 1 : 0);
 }
