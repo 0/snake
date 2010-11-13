0/snake

commit d6ebc30a781a853726d161697d68f85914f797e4

Added pausing

snake.c
@@ -21,6 +21,7 @@
 #define KEY_RIGHT_ALT 'd'
 #define KEY_DOWN_ALT  's'
 #define KEY_LEFT_ALT  'a'
+#define KEY_PAUSE     'p'
 #define KEY_QUIT      'q'
 
 #define GROWTH_MIN 10
@@ -69,6 +70,8 @@ typedef struct {
 
 time_t time_start, time_stop;
 unsigned int frame_wait;
+
+int paused = 0;
 
 direction_t dir;
 unsigned int len;
@@ -473,9 +476,10 @@ int main(int argc, char **argv) {
 	}
 
 	if (instructions_flag) {
-		mvaddstr(0, 0, "move: arrows or WASD");
-		mvaddstr(1, 0, "stop: Q or die");
-		mvaddstr(2, 0, "quit: any key");
+		mvaddstr(0, 0, "move:  arrows or WASD");
+		mvaddstr(1, 0, "stop:  Q or die");
+		mvaddstr(2, 0, "pause: P");
+		mvaddstr(3, 0, "quit:  any key");
 	}
 
 	attron(A_BOLD);
@@ -551,6 +555,14 @@ int main(int argc, char **argv) {
 					dir = WEST;
 					speedUp();
 					break;
+				case KEY_PAUSE:
+					paused = !paused;
+					if (paused)
+						mvaddstr(HEAD.y, HEAD.x, "PAUSED");
+					else
+						mvaddstr(HEAD.y, HEAD.x, "      ");
+
+					break;
 				case KEY_QUIT:
 					cause_of_death = DEATH_QUIT;
 					finish(0);
@@ -569,7 +581,7 @@ int main(int argc, char **argv) {
 			}
 		}
 
-		if (update) {
+		if (!paused && update) {
 			switch (moveSnake()) {
 				case 1:
 					if (rev)

commit 4bd0b35f8373c4c9f8d34d72bc65da6349dc7553

Fixed bug introduced in d6ebc30

snake.c
@@ -311,6 +311,32 @@ unsigned int speedUp(unsigned int frame_wait, unsigned int frame_min) {
 	return frame_wait;
 }
 
+void redraw(struct block *head, struct posn food, struct posn portal) {
+	struct block *b;
+
+	erase();
+
+	do_color(COLOR_LEAD, 1);
+	mvaddch(head->p.y, head->p.x, CH_HEAD);
+	do_color(COLOR_LEAD, 0);
+
+	b = head->next;
+	do_color(COLOR_BLOCK, 1);
+	while (b) {
+		mvaddch(b->p.y, b->p.x, CH_BODY);
+		b = b->next;
+	}
+	do_color(COLOR_BLOCK, 0);
+
+	do_color(COLOR_FOOD, 1);
+	mvaddch(food.y, food.x, CH_FOOD);
+	do_color(COLOR_FOOD, 0);
+
+	do_color(COLOR_PORTAL, 1);
+	mvaddch(portal.y, portal.x, CH_PORTAL);
+	do_color(COLOR_PORTAL, 0);
+}
+
 const char *generate_header() {
 	switch (rand() % 10) {
 		case 0:
@@ -601,7 +627,7 @@ int main(int argc, char **argv) {
 					if (paused)
 						mvaddstr(s.head->p.y, s.head->p.x, "PAUSED");
 					else
-						mvaddstr(s.head->p.y, s.head->p.x, "      ");
+						redraw(s.head, food, portal);
 
 					break;
 				case KEY_QUIT: